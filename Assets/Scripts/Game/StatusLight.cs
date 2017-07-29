c54dde9ba654b7fab2d02efe986ff26407e3a86f
Tom <tom@tomrijnbeek.nl>
2017-07-29 16:16:02 +0100

    Add status lights

---
+++ Assets/Scripts/Game/StatusLight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusLight : MonoBehaviour {
+
+    public Sprite OnLight;
+    public Sprite OffLight;
+    public Subsystem System;
+    public SpriteRenderer Renderer;
+
+	// Use this for initialization
+	void Start () {
+	    Renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (System.IsEnabled) {
+	        Renderer.sprite = OnLight;
+	    }
+	    else {
+	        Renderer.sprite = OffLight;
+	    }
+	}
+}
